432de99a0ffc69dce0b5b6b00f818cba9f8ec331
Mosa6 <tikamongi@gmail.com>
2022-04-17 09:22:58 +0000

    [Suspension]Bookers2

--- db/migrate/20220415003027_removeintroduction_from_users.rb
+++ db/migrate/20220415003027_removeintroduction_from_users.rb
@@ -1,4 +1,8 @@
 class RemoveintroductionFromUsers < ActiveRecord::Migration[6.1]
   def change
+       create_table :users do |t|
+      t.string :name
+      t.text :introduction
+    end
   end
 end
