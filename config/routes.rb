67cb07a2f7da0a313da69f0338afffd420621a7f
Mosa6 <tikamongi@gmail.com>
2022-04-19 13:22:25 +0000

    [Suspension]Bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "homes/about" => "homes#about"
   devise_for :users
-  resources :books, only: [:new, :create, :index, :show, :update, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :index, :edit, :update]
 end
--- db/migrate/20220415003027_removeintroduction_from_users.rb
+++ db/migrate/20220415003027_removeintroduction_from_users.rb
@@ -1,6 +1,6 @@
 class RemoveintroductionFromUsers < ActiveRecord::Migration[6.1]
   def change
-       create_table :users do |t|
+      create_table :users do |t|
       t.string :name
       t.text :introduction
     end
