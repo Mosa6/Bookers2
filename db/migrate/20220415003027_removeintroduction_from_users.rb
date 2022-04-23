61fe1d5f7dc13c34b2418ddcb878b17e2d47cef4
Mosa6 <tikamongi@gmail.com>
2022-04-23 11:25:29 +0000

    [Finish]Bookers2

--- db/migrate/20220415003027_removeintroduction_from_users.rb
+++
@@ -1,8 +0,0 @@
-class RemoveintroductionFromUsers < ActiveRecord::Migration[6.1]
-  def change
-      create_table :users do |t|
-      t.string :name
-      t.text :introduction
-    end
-  end
-end
